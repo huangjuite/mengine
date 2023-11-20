8f496ecd5bf5fce5633a520341340fdee1786dc8
huangjuite <huangjuite@gmail.com>
2023-11-20 18:25:22 -0500

    as5

--- assignments/assignment5/part1.py
+++ assignments/assignment5/part1.py
@@ -18,9 +18,21 @@ from scipy import optimize
 #########################################################################
 
 
-def contact_screw_3d(contact_points: np.ndarray, contact_normals: np.ndarray) -> np.ndarray:
+def contact_screw_3d(
+    contact_points: np.ndarray, contact_normals: np.ndarray
+) -> np.ndarray:
+    p = contact_points
+    # u = contact_normals
+    u = contact_normals / np.linalg.norm(contact_normals, axis=1, keepdims=1)
+
+    wrench = np.zeros((p.shape[0], 6))
+    wrench[:, :3] = u
+    wrench[:, 3:] = np.cross(p, u)
+
+    # wrench = wrench / np.linalg.norm(wrench, axis=1, keepdims=1)
 
     return wrench
+
 
 #########################################################################
 # N: the number of contact points; scalar
@@ -33,11 +45,67 @@ def contact_screw_3d(contact_points: np.ndarray, contact_normals: np.ndarray) ->
 #########################################################################
 
 
-def friction_cone_3d(contact_points: np.ndarray, contact_normals: np.ndarray, mu: float, n_fc: int) -> tuple:
-    #
-    # your code here
-    #
-    #
+def compute_rot_mat(n):
+    # tR = np.array([[0, 1, 0], [0, 0, 1], [1, 0, 0]])
+    # nx = n
+    # ny = tR @ nx
+    # nz = tR @ ny
+
+    ty = n + np.random.random(3)
+    nx = n
+    ny = ty - (ty @ nx) * nx
+    ny = ny / np.linalg.norm(ny)
+    nz = np.cross(nx, ny)
+
+    R = np.array([nx, ny, nz]).T
+
+    # print("------------------")
+    # print(R)
+    # print(nx @ ny, ny @ nz)
+    # print(np.linalg.det(R))
+    # print("==================")
+
+    return R
+
+
+def friction_cone_3d(
+    contact_points: np.ndarray, contact_normals: np.ndarray, mu: float, n_fc: int
+) -> tuple:
+    N = contact_points.shape[0]
+    contact_points_FC = []
+    contact_normals_FC = []
+
+    for i in range(N):
+        p = contact_points[i]
+        n = contact_normals[i]
+        n = n / np.linalg.norm(n)
+        new_n = np.zeros((n_fc, 3))
+        R = compute_rot_mat(n)
+        for k in range(n_fc):
+            new_n[k] = R @ np.array(
+                [
+                    1,
+                    mu * np.cos(2 * k * np.pi / n_fc),
+                    mu * np.sin(2 * k * np.pi / n_fc),
+                ]
+            )
+            contact_points_FC.append(p)
+            contact_normals_FC.append(new_n[k])
+
+        # import open3d as o3d
+        # pc = np.vstack(([p, p + n], p + new_n))
+        # pcd = o3d.geometry.PointCloud()
+        # pcd.points = o3d.utility.Vector3dVector(pc)
+
+        # lines = [[0, i] for i in range(1, n_fc + 2)]
+        # line_set = o3d.geometry.LineSet()
+        # line_set.points = o3d.utility.Vector3dVector(pc)
+        # line_set.lines = o3d.utility.Vector2iVector(lines)
+        # o3d.visualization.draw_geometries([pcd, line_set])
+
+    contact_points_FC = np.asarray(contact_points_FC)
+    contact_normals_FC = np.asarray(contact_normals_FC)
+
     return contact_points_FC, contact_normals_FC
 
 
@@ -47,19 +115,32 @@ def friction_cone_3d(contact_points: np.ndarray, contact_normals: np.ndarray, mu
 # z_max: maximum objective function value at optimal point; float
 #########################################################################
 def is_force_closure(w: np.ndarray) -> tuple:
+    r = np.linalg.matrix_rank(w)
+    print("rank: ", r)
+    if r < 6:
+        return False, 1
+
+    Wc = np.mean(w, axis=0)
+    c = Wc
+    b_ub = np.ones(w.shape[0])
+    A_ub = w - Wc
+    res = optimize.linprog(c, A_ub=A_ub, b_ub=b_ub)
+    # print(res.x, res.fun)
+    if res.fun is None:
+        return False, 1
+
+    z_max = -res.fun
+    is_FC = True if z_max < 1 else False
 
     return is_FC, z_max
 
 
 def main(testcase):
-
     if testcase == 0:
         # Case 0
-        contact_points = np.array(
-            [[2, 0, 0], [0, 1.5, 0], [0, 0, 2], [1.2, -2, 0]])
+        contact_points = np.array([[2, 0, 0], [0, 1.5, 0], [0, 0, 2], [1.2, -2, 0]])
         # inward-pointing contact normal direction
-        contact_normals = np.array(
-            [[-1, 0, 0], [0, -1, 0], [0, 0, -1], [0, 1, 0]])
+        contact_normals = np.array([[-1, 0, 0], [0, -1, 0], [0, 0, -1], [0, 1, 0]])
 
         # friction coefficient
         mu = 0.5
@@ -77,11 +158,19 @@ def main(testcase):
         n_friction_cone = 100
     elif testcase == 2:
         # Case 2
-        contact_points = np.array([[-0.81, 1.26, 1.23], [-1.75, -1.37, 0.74], [
-            1.49, 0.85, -1.39], [0.49, -2.57, -0.39], [1.23, -1.04, 1.20]])
+        contact_points = np.array(
+            [
+                [-0.81, 1.26, 1.23],
+                [-1.75, -1.37, 0.74],
+                [1.49, 0.85, -1.39],
+                [0.49, -2.57, -0.39],
+                [1.23, -1.04, 1.20],
+            ]
+        )
         # % relative rotation matrix(first column in R, i.e., x-axis of local frame, is the pushing direction)
         contact_normals = np.array(
-            [[0, -1, 0], [1, 0, 0], [0, -1, 0], [0, 1, 0], [0, 0, 1]])
+            [[0, -1, 0], [1, 0, 0], [0, -1, 0], [0, 1, 0], [0, 0, 1]]
+        )
         # friction coefficient
         mu = 0.2
 
@@ -89,13 +178,14 @@ def main(testcase):
         n_friction_cone = 100
     else:
         raise IndexError(
-            "Test case does not exist. Provide either 0,1 or 2 as input argument.")
+            "Test case does not exist. Provide either 0,1 or 2 as input argument."
+        )
 
     # normalized screw coordinates of contact normals
     wrench = contact_screw_3d(contact_points, contact_normals)
 
     # draw contact screws
-    draw_contact_screw(contact_points, wrench)
+    # draw_contact_screw(contact_points, wrench)
 
     # force closure test (1: true, 0: false)
     is_FC, z_max = is_force_closure(wrench)
@@ -104,30 +194,40 @@ def main(testcase):
 
     # friction cone approximation
     contact_points_FC, contact_normals_FC = friction_cone_3d(
-        contact_points, contact_normals, mu, n_friction_cone)
+        contact_points, contact_normals, mu, n_friction_cone
+    )
 
     # normalized screw coordinates of contact normals
     wrench_friction = contact_screw_3d(contact_points_FC, contact_normals_FC)
 
     # draw contact screw
-    draw_contact_screw(contact_points_FC, contact_normals_FC, n_friction_cone)
+    # draw_contact_screw(contact_points_FC, contact_normals_FC, n_friction_cone)
 
     # force closure test (1: true, 0: false)
     is_FC_friction, z_max_friction = is_force_closure(wrench_friction)
 
     # print results
-    print('force closure without friction:', is_FC,
-          '  maximum objective function value: ', z_max)
-    print('force closure with friction:', is_FC_friction,
-          '  maximum objective function value: ', z_max_friction)
+    print(
+        "force closure without friction:",
+        is_FC,
+        "  maximum objective function value: ",
+        z_max,
+    )
+    print(
+        "force closure with friction:",
+        is_FC_friction,
+        "  maximum objective function value: ",
+        z_max_friction,
+    )
 
 
 if __name__ == "__main__":
-
-    parser = argparse.ArgumentParser(description='Parse different test cases.')
-    parser.add_argument('testcase', type=int,
-
-                        help='Call file with an index for the respective test cases (0: an example from references [1] and [2], 1+2: are test cases for grading)')
+    parser = argparse.ArgumentParser(description="Parse different test cases.")
+    parser.add_argument(
+        "testcase",
+        type=int,
+        help="Call file with an index for the respective test cases (0: an example from references [1] and [2], 1+2: are test cases for grading)",
+    )
     try:
         args = parser.parse_args()
         testcase = args.testcase
--- assignments/assignment5/part2.py
+++ assignments/assignment5/part2.py
@@ -1,8 +1,9 @@
 from imp import lock_held
 import os
+import time
 import numpy as np
 import mengine as m
-
+import copy
 
 # use the functions created in part1 of the homework to find a force closure grasp
 from part1 import friction_cone_3d, contact_screw_3d, is_force_closure
@@ -15,62 +16,153 @@ orient = m.get_quaternion([np.pi, 0, 0])
 
 
 def sample_spherical(npoints, ndim=3):
-    # write your code here
-    #
-    #
-    return points
+    p = np.random.random((npoints, 3)) - 0.5
+    points = 0.1 * p / np.linalg.norm(p, axis=1, keepdims=1)
+    normal = -points
+
+    # import open3d as o3d
+    # pcd = o3d.geometry.PointCloud()
+    # pcd.points = o3d.utility.Vector3dVector(points)
+    # o3d.visualization.draw_geometries([pcd])
+
+    return points, normal
+
+
+Rsample = np.array(
+    [
+        [[1, 0, 0], [0, 1, 0], [0, 0, 1]],
+        [[0, -1, 0], [1, 0, 0], [0, 0, 1]],
+        [[-1, 0, 0], [0, -1, 0], [0, 0, 1]],
+        [[0, 1, 0], [-1, 0, 0], [0, 0, 1]],
+        [[0, 0, -1], [0, 1, 0], [1, 0, 0]],
+        [[0, 0, 1], [0, 1, 0], [-1, 0, 0]],
+    ]
+)
 
 
 def sample_cube(npoints, ndim=3):
-    # write your code here
-    #
-    #
+    points, normals = [], []
+    for n in range(npoints):
+        p = np.random.uniform(-0.1, 0.1, (3))
+        p[0] = 0.1
+        f = np.random.randint(0, Rsample.shape[0])
+        # print(f)
+        p = Rsample[f] @ p
+        points.append(p)
+        n = np.array([-1, 0, 0])
+        normals.append(Rsample[f] @ n)
 
-    return points
+    points = np.asarray(points)
+    normals = np.asarray(normals)
+    # print(points, normals)
+
+    # import open3d as o3d
+    # pcd = o3d.geometry.PointCloud()
+    # pcd.points = o3d.utility.Vector3dVector(points)
+    # o3d.visualization.draw_geometries([pcd])
+
+    return points, normals
 
 
 def find_force_closure_grasp(testobj, mu) -> tuple:
+    max_iter, iter = 100, 0
+    while True:
+        iter += 1
+        if testobj == "sphere":
+            points, normals = sample_spherical(3)
+        else:
+            points, normals = sample_cube(6)
 
-    # write your code here
-    #
-    #
+        if mu != 0:
+            contact_points_FC, contact_normals_FC = friction_cone_3d(
+                points, normals, mu, 100
+            )
+        else:
+            contact_points_FC, contact_normals_FC = copy.deepcopy(
+                points
+            ), copy.deepcopy(normals)
 
-    return contact_positions, contact_normals
+        # print(contact_points_FC.shape)
+        wrench_friction = contact_screw_3d(contact_points_FC, contact_normals_FC)
+        is_FC_friction, z_max_friction = is_force_closure(wrench_friction)
+
+        print(is_FC_friction, z_max_friction)
+        if is_FC_friction:
+            break
+        
+        if iter >= max_iter:
+            print('no force closure grasp available')
+            break
+
+    return points, normals
 
 
 # Reset simulation env
 
 
-def reset(positions, table_friction=0.5, obj_mass=100, obj_friction=0.5, finger_mass=10.0, obj_type='sphere'):
+def reset(
+    positions,
+    table_friction=0.5,
+    obj_mass=100,
+    obj_friction=0.5,
+    finger_mass=10.0,
+    obj_type="sphere",
+):
     # Create environment and ground plane
     env.reset()
     ground = m.Ground()
 
     # Create table and cube
-    table = m.URDF(filename=os.path.join(m.directory, 'table', 'table.urdf'), static=True, position=[
-                   0, 0, 0], orientation=[0, 0, 0, 1], maximal_coordinates=True, scale=1.0)
+    table = m.URDF(
+        filename=os.path.join(m.directory, "table", "table.urdf"),
+        static=True,
+        position=[0, 0, 0],
+        orientation=[0, 0, 0, 1],
+        maximal_coordinates=True,
+        # scale=1.0,
+    )
     table.set_whole_body_frictions(
-        lateral_friction=table_friction, spinning_friction=0, rolling_friction=0)
-    if obj_type == 'sphere':
-        obj = m.Shape(m.Sphere(radius=0.1), static=False, mass=obj_mass,
-                      position=[0, 0, 1.2], rgba=[0, 1, 0, 1])
-    elif obj_type == 'cube':
-        obj = m.Shape(m.Box(half_extents=[0.1]*3), static=False, mass=obj_mass, position=[
-            0, 0, 1.2], orientation=[0, 0, 0, 1], rgba=[0, 1, 0, 1])
+        lateral_friction=table_friction, spinning_friction=0, rolling_friction=0
+    )
+    if obj_type == "sphere":
+        obj = m.Shape(
+            m.Sphere(radius=0.1),
+            static=False,
+            mass=obj_mass,
+            position=[0, 0, 1.2],
+            rgba=[0, 1, 0, 1],
+        )
+    elif obj_type == "cube":
+        obj = m.Shape(
+            m.Box(half_extents=[0.1] * 3),
+            static=False,
+            mass=obj_mass,
+            position=[0, 0, 1.2],
+            orientation=[0, 0, 0, 1],
+            rgba=[0, 1, 0, 1],
+        )
     else:
-        raise TypeError(
-            'Object Type unknown. Use either sphere or cube as obj.')
+        raise TypeError("Object Type unknown. Use either sphere or cube as obj.")
 
     obj.set_whole_body_frictions(
-        lateral_friction=obj_friction, spinning_friction=0, rolling_friction=0)
+        lateral_friction=obj_friction, spinning_friction=0, rolling_friction=0
+    )
 
     # Create n-spheres(fingers) to make contact with the cube.
     fingers = []
     for i, p in enumerate(positions):
-        fingers.append(m.Shape(m.Sphere(radius=0.02), static=False, mass=finger_mass,
-                               position=obj.local_to_global_coordinate_frame(positions[i])[0], rgba=[1, 0, 0, 1]))
+        fingers.append(
+            m.Shape(
+                m.Sphere(radius=0.02),
+                static=False,
+                mass=finger_mass,
+                position=obj.local_to_global_coordinate_frame(positions[i])[0],
+                rgba=[1, 0, 0, 1],
+            )
+        )
         fingers[i].set_whole_body_frictions(
-            lateral_friction=0.5, spinning_friction=100, rolling_friction=100)
+            lateral_friction=0.5, spinning_friction=100, rolling_friction=100
+        )
     return fingers, obj
 
 
@@ -80,33 +172,47 @@ def reset(positions, table_friction=0.5, obj_mass=100, obj_friction=0.5, finger_
 def visualize_grasps(grasps):
     for g in grasps:
         # Reset simulator
-        fingers, obj = reset(g['contact_positions'],  g['table_friction'],
-                             g['obj_mass'], g['obj_friction'], g['finger_mass'])
+        fingers, obj = reset(
+            g["contact_positions"],
+            g["table_friction"],
+            g["obj_mass"],
+            g["obj_friction"],
+            g["finger_mass"],
+            g["obj_type"],
+        )
+
         for i in range(100):
+            m.clear_all_visual_items()
 
             for idx, finger in enumerate(fingers):
-
                 # Gravity compensation force
-                force = -finger.get_link_mass(finger.base)*env.gravity
+                force = -finger.get_link_mass(finger.base) * env.gravity
                 # Apply force in contact point in the direction of the contact normal
-                force += np.array(g['force_magnitude'] *
-                                  g['contact_normals'][idx])
-                finger.apply_external_force(link=finger.base, force=force, pos=finger.get_base_pos_orient()[
-                                            0], local_coordinate_frame=False)
+                force += np.array(g["force_magnitude"] * g["contact_normals"][idx])
+                finger.apply_external_force(
+                    link=finger.base,
+                    force=force,
+                    pos=finger.get_base_pos_orient()[0],
+                    local_coordinate_frame=False,
+                )
+
+                # Show contact normals
+                cp = finger.get_contact_points(bodyB=obj)
+                if cp is not None:
+                    c = cp[0]
+                    m.Line(
+                        c["posB"],
+                        np.array(c["posB"]) + np.array(c["contact_normal"]) * 0.2,
+                        rgb=[1, 0, 0],
+                    )
 
             m.step_simulation()
+            # time.sleep(0.5)
 
-            # Show contact normals
-            cp = finger.get_contact_points(bodyB=obj)
-            m.clear_all_visual_items()
-            if cp is not None:
-                for c in cp:
-                    line = m.Line(c['posB'], np.array(c['posB']) +
-                                  np.array(c['contact_normal'])*0.2, rgb=[1, 0, 0])
+            
 
 
 def main(testobj, friction=True):
-
     # parameters to play with
     obj_mass = 100
     obj_friction = 0.5
@@ -117,12 +223,21 @@ def main(testobj, friction=True):
     else:
         mu = 0.0
 
-    contact_positions, contact_normals = find_force_closure_grasp(
-        testobj, mu)
+    contact_positions, contact_normals = find_force_closure_grasp(testobj, mu)
 
     # spawn fingers slightly away from the obj
-    grasps = [dict(contact_positions=1.1*contact_positions, contact_normals=contact_normals,
-                   table_friction=0.5, obj_type=testobj, obj_mass=obj_mass, obj_friction=obj_friction, force_magnitude=force_magnitude, finger_mass=finger_mass)]
+    grasps = [
+        dict(
+            contact_positions=1.1 * contact_positions,
+            contact_normals=contact_normals,
+            table_friction=0.5,
+            obj_type=testobj,
+            obj_mass=obj_mass,
+            obj_friction=obj_friction,
+            force_magnitude=force_magnitude,
+            finger_mass=finger_mass,
+        )
+    ]
 
     # append grasps with other parameters here:
     # grasps.append()
@@ -132,20 +247,23 @@ def main(testobj, friction=True):
 
 if __name__ == "__main__":
     import argparse
-    parser = argparse.ArgumentParser(
-        description='Parse different test objects.')
-    parser.add_argument('testobject', type=str,
 
-                        help='Call file either with argument \'sphere\' or \'cube\'.')
-    parser.add_argument('friction', type=bool,
-
-                        help='Frictionless => False , frictional=>True')
+    parser = argparse.ArgumentParser(description="Parse different test objects.")
+    parser.add_argument(
+        "testobject",
+        type=str,
+        help="Call file either with argument 'sphere' or 'cube'.",
+    )
+    parser.add_argument(
+        "friction", type=int, help="Frictionless => False , frictional=>True"
+    )
     try:
         args = parser.parse_args()
-        testobj = args.testobj
+        testobj = args.testobject
         friction = args.friction
+        print(friction)
     except:
         print("No testobj specified. Using sphere as default.")
-        testobj = 'sphere'
+        testobj = "sphere"
         friction = False
     main(testobj, friction)
